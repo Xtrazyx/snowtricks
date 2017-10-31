fd1097f2a26ef8270f598be8bfb5b5e9e05f4b71
Xtrazyx <xtrazyx@gmail.com>
2017-10-31 01:06:39 +0100

    Reset password mail

---
+++ src/Form/ChangePasswordType.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Xtrazyx
+ * Date: 29/09/2017
+ * Time: 16:16
+ */
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class ChangePasswordType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('password', RepeatedType::class, array(
+                'type' => PasswordType::class,
+                'first_options' => array('label' => 'Mot de passe'),
+                'second_options' => array('label' => 'Répéter le mot de passe')
+            ))
+        ;
+    }
+}
